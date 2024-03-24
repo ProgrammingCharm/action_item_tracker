4bae56611714e47d7b20d9c523122d3a9860dfb2
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-03-24 13:14:44 -0700

    Update app.py
    When adding an action item, a checkbox input type has been added in. When the checkbox is toggled, it is picked up by the route handler to mark this action item as completed. If it is not marked as complete (left unchecked), it will be stored as "not complete". 
    
    I also present a handling route for when the user is adding a new user to the database.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from dml import add_meeting, add_user, add_action_item, get_all_meetings
+from dml import add_meeting, add_user, add_action_item, get_all_meetings, get_all_users
 from flask import Flask, render_template, request
 import sqlite3
 
@@ -19,17 +19,18 @@ def index():
 			username = request.form['username']
 			add_user(username)
 		# Check if form is for adding an action item
-		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form and 'owner' in request.form:
+		elif 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form and 'owner' in request.form:
 			meeting_id = request.form['meeting_id']
 			user_id = request.form['user_id']
 			item = request.form['item']
-			completion = request.form['completion']
+			completion = request.form.get('completed', 'not completed')
 			notes = request.form['notes']
 			owner = request.form['owner']
 			add_action_item(meeting_id, user_id, item, completion, notes, owner)
 	
 	meetings = get_all_meetings()
-	return render_template('index.html', meetings=meetings)
+	users = get_all_users()
+	return render_template('index.html', meetings=meetings, users=users)
 
 if __name__ == '__main__':
 	app.run(debug=True)
