188259b4cc2d2fe0c2234b952bc58ae8edae105d
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-03-26 12:38:31 -0700

    Update app.py
    Allows for the additional route add_action_item to take place once a user has pressed the button on the homepage. Then gives way to adding an action item on a separate static page specifically designed for adding an action item.

--- app.py
+++ app.py
@@ -18,20 +18,26 @@ def index():
 		elif 'username' in request.form:
 			username = request.form['username']
 			add_user(username)
-		# Check if form is for adding an action item
-		elif 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form and 'owner' in request.form:
-			meeting_id = request.form['meeting_id']
-			user_id = request.form['user_id']
-			item = request.form['item']
-			completion = request.form.get('completed', 'not completed')
-			notes = request.form['notes']
-			owner = request.form['owner']
-			add_action_item(meeting_id, user_id, item, completion, notes, owner)
-	
 	meetings = get_all_meetings()
 	users = get_all_users()
 	return render_template('index.html', meetings=meetings, users=users)
 
+@app.route('/add_action_item', methods=['GET', 'POST'])
+def action_item():
+	if request.method == 'POST':
+		# If form is for adding an action item
+		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form:
+			meeting_id = request.form['meeting_id']
+			user_id = request.form['user_id']
+			item = request.form['item']
+			completion = 'completed' if 'completion' in request.form else 'not completed'
+			notes = request.form['notes']
+			add_action_item(meeting_id, user_id, item, completion, notes)
+	meetings = get_all_meetings()
+	users = get_all_users()
+	return render_template('add_action_item.html', meetings=meetings, users=users)
+	
+	
 if __name__ == '__main__':
 	app.run(debug=True)
 	
