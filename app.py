114ed034e450ce0daea918a82e9d9c5acc9fe63c
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-04-10 13:27:23 -0700

    Update app.py
    Removed date from meeting so app.py did not need this argument for the add_meeting_user() function.

--- app.py
+++ app.py
@@ -9,35 +9,35 @@ app = Flask(__name__, template_folder="templates")
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
+		# If form is for adding an action item
+		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form:
+			meeting_id = request.form['meeting_id']
+			user_id = request.form['user_id']
+			item = request.form['item']
+			completion = request.form.get('completion', 'not completed')
+			notes = request.form['notes']
+			add_action_item(meeting_id, user_id, item, completion, notes)
+	meetings = get_all_meetings()
+	users = get_all_users()
+	return render_template('index.html', meetings=meetings, users=users)
+
+@app.route('/settings', methods=['GET', 'POST'])
+def add_meeting_user():
+	if request.method == 'POST':
 		# Check if form is for adding a meeting
-		if 'name' in request.form and 'date' in request.form:
+		if 'name' in request.form:
 			name = request.form['name']
-			date = request.form['date']
-			add_meeting(name, date)
+			add_meeting(name)
 		# Check if form is for adding a user
 		elif 'username' in request.form:
 			username = request.form['username']
 			add_user(username)
 	meetings = get_all_meetings()
 	users = get_all_users()
-	return render_template('index.html', meetings=meetings, users=users)
-
-@app.route('/add_action_item', methods=['GET', 'POST'])
-def action_item():
-	if request.method == 'POST':
-		# If form is for adding an action item
-		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'completion' in request.form and 'notes' in request.form:
-			meeting_id = request.form['meeting_id']
-			user_id = request.form['user_id']
-			item = request.form['item']
-			completion = 'completed' if 'completion' in request.form else 'not completed'
-			notes = request.form['notes']
-			add_action_item(meeting_id, user_id, item, completion, notes)
-	meetings = get_all_meetings()
-	users = get_all_users()
-	return render_template('add_action_item.html', meetings=meetings, users=users)
+	return render_template('settings.html', meetings=meetings, users=users)
 	
 	
 if __name__ == '__main__':
 	app.run(debug=True)
 	
+
