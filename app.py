5ab303adb5e2a0e348059306de629e550af69a15
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-05-21 13:54:10 -0700

    Update app.py
    Filter routes for filtering by user or meeting.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from dml import add_meeting, add_user, add_action_item, get_all_meetings, get_all_users
+from dml import add_meeting, add_user, add_action_item, get_all_meetings, get_all_users, get_all_action_items, get_id_by_meeting_name, get_action_items_by_meeting, get_id_by_user_name, get_action_items_by_user
 from flask import Flask, render_template, request
 import sqlite3
 
@@ -19,16 +19,38 @@ def index():
 			add_action_item(meeting_id, user_id, item, completion, notes)
 	meetings = get_all_meetings()
 	users = get_all_users()
-	return render_template('index.html', meetings=meetings, users=users)
-
+	action_items = get_all_action_items()
+	return render_template('index.html', meetings=meetings, users=users, action_items=action_items)
+	
+@app.route('/filter_by_meeting', methods=['POST'])
+def filter_by_meeting():
+	meeting_name = request.form['meeting_name']
+	#print(f"Meeting Name: {meeting_name}")
+	meeting_id = get_id_by_meeting_name(meeting_name)
+	#print(f"Meeting ID: {meeting_id}")
+	#print(f"Meeting Name: {meeting_name}")
+	action_items = get_action_items_by_meeting(meeting_id)
+	#print(f"Action Items: {action_items}")
+	return render_template('index.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items)
+	
+@app.route('/filter_by_user', methods=["POST"])
+def filter_by_user():
+	user_name = request.form['user_name']
+	print(f"User Name: {user_name}")
+	user_id = get_id_by_user_name(user_name)
+	print(f"User ID: {user_id}")
+	action_items = get_action_items_by_user(user_id)
+	print(f"Action Items: {action_items}")
+	return render_template('index.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items)
+	
+	
 @app.route('/settings', methods=['GET', 'POST'])
 def add_meeting_user():
 	if request.method == 'POST':
 		# Check if form is for adding a meeting
-		if 'name' in request.form and 'date' in request.form:
+		if 'name' in request.form:
 			name = request.form['name']
-			date = request.form['date']
-			add_meeting(name, date)
+			add_meeting(name)
 		# Check if form is for adding a user
 		elif 'username' in request.form:
 			username = request.form['username']
@@ -42,4 +64,4 @@ if __name__ == '__main__':
 	app.run(debug=True)
 	
 
-	
+
