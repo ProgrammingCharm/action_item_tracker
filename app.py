4046e57aaff3ac3ee7abe246e41a51a5d5366205
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-06-20 12:57:45 -0700

    Update app.py
    When an action item is added through the index route, the init_note_action_item() function is called. When a note is appended to an action item, the route add_note adds a timestamp as well as the note. Allowing for organized notes in the viewing front-end. action_items_page returns a new parameter called completed_action_items which calls the get_all_completed_action_items() which is important for displaying the new table being implemented in action_items.html for Completed Action Items so a user of the web application can now view which action items are complete and are not complete all on the same page.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# app.py
+x# app.py
 
-from dml import add_meeting, add_user, add_action_item, get_all_meetings, get_all_users, get_all_action_items, get_id_by_meeting_name, get_action_items_by_meeting, get_id_by_user_name, get_action_items_by_user, update_action_item_completion, append_note_to_action_item, get_id_by_action_item_name, get_timestamp, append_timestamp_to_date
+from dml import add_meeting, add_user, add_action_item, get_all_meetings, get_all_users, get_all_action_items, get_id_by_meeting_name, get_action_items_by_meeting, get_id_by_user_name, get_action_items_by_user, update_action_item_completion, append_note_to_action_item, get_id_by_action_item_name, get_timestamp, init_note_action_item, termination_note_action_item, get_all_completed_action_items
 from flask import Flask, render_template, request
 import sqlite3
 
@@ -9,15 +9,17 @@ app = Flask(__name__, template_folder="templates")
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
+		#Temporarily removing date as field
 		# If form is for adding an action item
-		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form and 'notes' in request.form:
+		if 'meeting_id' in request.form and 'user_id' in request.form and 'item' in request.form:
 			meeting_id = request.form['meeting_id']
 			user_id = request.form['user_id']
 			item = request.form['item']
 			completion = 'not completed'
-			notes = request.form['notes']
-			date = get_timestamp()
-			add_action_item(meeting_id, user_id, item, completion, notes, date)
+			notes = init_note_action_item()
+			#notes = request.form['notes']
+			#date = get_timestamp()
+			add_action_item(meeting_id, user_id, item, completion, notes) #, date
 	meetings = get_all_meetings()
 	users = get_all_users()
 	action_items = get_all_action_items()
@@ -25,26 +27,29 @@ def index():
 	
 @app.route('/action_items', methods = ['GET'])
 def action_items_page():
-	return render_template('action_items.html', meetings = get_all_meetings(), users = get_all_users(), action_items = get_all_action_items())
+	return render_template('action_items.html', meetings = get_all_meetings(), users = get_all_users(), action_items = get_all_action_items(), completed_action_items = get_all_completed_action_items())
 	
 @app.route('/filter_by_meeting', methods=['POST'])
 def filter_by_meeting():
 	meeting_name = request.form['meeting_name']
 	meeting_id = get_id_by_meeting_name(meeting_name)
 	action_items = get_action_items_by_meeting(meeting_id)
-	return render_template('action_items.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items)
+	return render_template('action_items.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items, completed_action_items=get_all_completed_action_items())
 	
 @app.route('/filter_by_user', methods=["POST"])
 def filter_by_user():
 	user_name = request.form['user_name']
 	user_id = get_id_by_user_name(user_name)
 	action_items = get_action_items_by_user(user_id)
-	return render_template('action_items.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items)
+	return render_template('action_items.html', meetings=get_all_meetings(), users=get_all_users(), action_items=action_items, completed_action_items=get_all_completed_action_items())
 	
 @app.route('/mark_complete', methods=['POST'])
 def mark_complete():
 	completed_ids = request.form.getlist('completed_ids')
 	update_action_item_completion(completed_ids)
+	for action_item_id in completed_ids:
+		termination_note = termination_note_action_item()
+		append_note_to_action_item(action_item_id, termination_note)
 	return action_items_page()
 
 @app.route('/settings', methods=['GET', 'POST'])
@@ -68,16 +73,13 @@ def add_note():
 	action_item_name = request.form['action_item_name']
 	new_note = request.form['new_note']
 	action_item_id = get_id_by_action_item_name(action_item_name)
+	timestamp = get_timestamp()
+	new_note = f"[{timestamp}] {new_note}"
 	append_note_to_action_item(action_item_id, new_note)
-	date_time = get_timestamp()
-	append_timestamp_to_date(action_item_id, date_time)
 	return action_items_page()
 	
 	
 if __name__ == '__main__':
 	app.run(debug=True)
 	
-
-
-
 
