902ebe0ab76398b6d40c285368ab5a3be14c9a42
Jonah G <122331039+ProgrammingCharm@users.noreply.github.com>
2024-07-04 15:05:08 -0700

    Update ddl.py
    Moved my web application to a hosting platform called PythonAnywhere. After editing the provided WSGI file, I encapsulated all of my SQLite CREATE TABLE commands into one function that can be called from within my app.py file. This works well because my app.py file will check to see if the database schema has already been set up, if it has, then nothing needs to happen. It checks the file path on PythonAnywhere's server to see if the schema currently exists.

--- ddl.py
+++ ddl.py
@@ -6,44 +6,45 @@
 
 import sqlite3
 
-conn = sqlite3.connect("action_item_tracker.db")
-cursor = conn.cursor()
+def initialize_database():
+	conn = sqlite3.connect("action_item_tracker.db")
+	cursor = conn.cursor()
 
-cursor.execute(
-	"""
-	CREATE TABLE IF NOT EXISTS meetings(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT
+	cursor.execute(
+		"""
+		CREATE TABLE IF NOT EXISTS meetings(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT
+		)
+		"""
 	)
-	"""
-)
 
-cursor.execute(
-	"""
-	CREATE TABLE IF NOT EXISTS users(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT
+	cursor.execute(
+		"""
+		CREATE TABLE IF NOT EXISTS users(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT
+		)
+		"""
+	)		
+
+	cursor.execute(
+		"""
+		CREATE TABLE IF NOT EXISTS action_items(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			meeting_id INTEGER,
+			user_id INTEGER,
+			meeting_name TEXT,
+			user_name TEXT,
+			item TEXT,
+			completion TEXT,
+			notes TEXT,
+			date TIMESTAMP,
+			FOREIGN KEY (meeting_id) REFERENCES meetings(id),
+			FOREIGN KEY (user_id) REFERENCES users(id)
+		)
+		"""
 	)
-	"""
-)		
 
-cursor.execute(
-	"""
-	CREATE TABLE IF NOT EXISTS action_items(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		meeting_id INTEGER,
-		user_id INTEGER,
-		meeting_name TEXT,
-		user_name TEXT,
-		item TEXT,
-		completion TEXT,
-		notes TEXT,
-		date TIMESTAMP,
-		FOREIGN KEY (meeting_id) REFERENCES meetings(id),
-		FOREIGN KEY (user_id) REFERENCES users(id)
-	)
-	"""
-)
-
-conn.commit()
-conn.close()
+	conn.commit()
+	conn.close()
